eba79ae8a49b6e0b873f12d7042d65ac73b153fe
Jaime Chung <jaime.abdiel@gmail.com>
2024-06-01 16:37:21 -0500

    Update CargarData.py

--- CargarData.py
+++ CargarData.py
@@ -3,24 +3,24 @@ from langchain_community.document_loaders import WebBaseLoader, PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def extraer_paginas(nombrepdf):
-	try:
-	  loader = PyPDFLoader(nombrepdf)
-    pages = loader.load_and_split()
-    print(f"Se cargaron {len(pages)} paginas del pdf {nombrepdf}")
-    return pages
-  except Exception as e:
-    print(f"Error error cargando PDF, razon: {e}"​)
+    try:
+        loader = PyPDFLoader(nombrepdf)
+        pages = loader.load_and_split()
+        print(f"Se cargaron {len(pages)} paginas del pdf {nombrepdf}")
+        return pages
+    except Exception as e:
+        print(f"Error error cargando PDF, razon: {e}")
 
-def separar_paginas(paginas, dimension):​
-	​try:
-	  text_splitter = RecursiveCharacterTextSplitter(chunk_size=dimension, ​chunk_overlap=200, add_start_index=True)
-	  chunks = text_splitter.split_documents(paginas)
-	  print(f"Se separaron {len(paginas)} paginas en {len(chunks)} pedazos")
+def separar_paginas(paginas, dimension):
+    try:
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=dimension, chunk_overlap=200, add_start_index=True)
+        chunks = text_splitter.split_documents(paginas)
+        print(f"Se separaron {len(paginas)} paginas en {len(chunks)} pedazos")
+        return chunks
+    except Exception as e:
+        print(f"Error separando pedazos, razon: {e}")
+
+def obtener_pedazos(nombre_pdf):
+    docs = extraer_paginas(nombre_pdf)
+    chunks = separar_paginas(docs, 500)
     return chunks
-  except Exception as e:
-    print(f"Error separando pedazos, razon: {e}")
-
-​def obtener_pedazos(nombre_pdf):
-  docs = extraer_paginas(nombre_pdf)
-  chunks = separar_paginas(docs, 500)
-	​return chunks
